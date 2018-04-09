d0151c531912809cc8eea21c8c4e73a42d493ec5
Daan DL <daandelombaert@gmail.com>
2018-04-09 16:44:01 +0200

    added google cloud translations api + callback

--- server/assistant/assistant.service.js
+++ server/assistant/assistant.service.js
@@ -1,10 +1,22 @@
 exports.process = async function (params) {
 
   const projectId = 'timecreditagent';
+  const translationProjectId = 'arcelormittal-hr-chatbot'
   const sessionId = 'quickstart-session-id';
   const query = params.message;
   const languageCode = 'en-US';
+
   const dialogflow = require('dialogflow');
+
+  // Imports the Google Cloud client library
+  const Translate = require('@google-cloud/translate');
+
+  // Instantiates a client
+  const translate = new Translate({
+    projectId: translationProjectId,
+    key: 'AIzaSyD5oXUYpsppyV-K8DtOdHXLytTsmJyclUE'
+  });
+
   let sessionClient;
 
   if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
@@ -22,35 +34,47 @@ exports.process = async function (params) {
   // Define session path
   const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-  // The text query request.
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
-        text: query,
-        languageCode: languageCode,
-      },
-    },
-  };
+  return translate
+    .translate(query, 'en')
+    .then(results => {
+      const translation = results[0];
+      // The text query request.
+      const request = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            text: translation,
+            languageCode: languageCode,
+          },
+        },
+      };
 
-  // Send request and log result
-  return sessionClient
-    .detectIntent(request)
-    .then(responses => {
-      params.type = 0;
-      console.log('Detected intent');
-      const result = responses[0].queryResult;
-      console.log(`  Query: ${result.queryText}`);
-      console.log(`  Response: ${result.fulfillmentText}`);
-      if (result.intent) {
-        params.message = result.fulfillmentText;
-        console.log(`  Intent: ${result.intent.displayName}`);
-      } else {
-        console.log(`  No intent matched.`);
-      }
-      return params;
+      // Send request and log result
+      return sessionClient
+        .detectIntent(request)
+        .then(responses => {
+          params.type = 0;
+          console.log('Detected intent');
+          const result = responses[0].queryResult;
+          console.log(`  Query: ${result.queryText}`);
+          console.log(`  Response: ${result.fulfillmentText}`);
+          if (result.intent) {
+            params.message = result.fulfillmentText;
+            console.log(`  Intent: ${result.intent.displayName}`);
+          } else {
+            console.log(`  No intent matched.`);
+          }
+          return params;
+        })
+        .catch(err => {
+          console.error('ERROR:', err);
+        });
+      console.log(`Text: ${text}`);
+      console.log(`Translation: ${translation}`);
     })
     .catch(err => {
       console.error('ERROR:', err);
     });
+
+
 }
