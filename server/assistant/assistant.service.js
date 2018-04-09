5b6f5a709bbe9b5301d78cf476b7c9c49427670a
Daan DL <daandelombaert@gmail.com>
2018-04-09 14:13:43 +0200

    small dialogflow response tweaks

--- server/assistant/assistant.service.js
+++ server/assistant/assistant.service.js
@@ -4,10 +4,6 @@ exports.process = async function (params) {
   const sessionId = 'quickstart-session-id';
   const query = params.message;
   const languageCode = 'en-US';
-  // let reply = params;
-  // reply.message = 'I have no answers yet :(,\nyour message was: ' + params.message;
-  // reply.type = 0;
-  // Instantiate a DialogFlow client.
   const dialogflow = require('dialogflow');
   const sessionClient = new dialogflow.SessionsClient();
 
@@ -29,25 +25,20 @@ exports.process = async function (params) {
   return sessionClient
     .detectIntent(request)
     .then(responses => {
+      params.type = 0;
       console.log('Detected intent');
       const result = responses[0].queryResult;
       console.log(`  Query: ${result.queryText}`);
       console.log(`  Response: ${result.fulfillmentText}`);
       if (result.intent) {
+        params.message = result.fulfillmentText;
         console.log(`  Intent: ${result.intent.displayName}`);
       } else {
         console.log(`  No intent matched.`);
       }
-      return result;
+      return params;
     })
     .catch(err => {
       console.error('ERROR:', err);
     });
-
-
-  // return new Promise(resolve => {
-  //   setTimeout(() => {
-  //     resolve(reply);
-  //   }, 2000);
-  // });;
 }
